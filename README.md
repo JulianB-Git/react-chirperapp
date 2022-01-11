da34fff9dd0bc2fa085eb5a55709ee6073b48959
Julian Benade <julian.benade@gmail.com>
2022-01-11 21:00:02 +0200

    Final code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-place holder
+# Chirper Project
+
+Twitter clone React Nanodegree course Udacity
+
+## Project Setup
+
+* clone the Project - `git@github.com:udacity/reactnd-chirper-app.git`
+* install the dependencies - `npm install`
---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { handleGetInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
+import LoadingBar from 'react-redux-loading'
+import NewTweet from "./NewTweet";
+import TweetPage from "./TweetPage";
+import Nav from "./Nav";
+
+class App extends Component {
+
+  componentDidMount() {
+    this.props.dispatch(handleGetInitialData())
+  }
+
+  render() {
+    return (
+        <Router>
+            <Fragment>
+                <LoadingBar />
+                <div id='root' className='container'>
+                    <Nav />
+                    {this.props.loading === true
+                        ? null
+                        : <div>
+                            <Route path='/' exact component={Dashboard} />
+                            <Route path='/tweet/:id' component={TweetPage} />
+                            <Route path='/new' component={NewTweet} />
+                        </div>}
+                </div>
+            </Fragment>
+        </Router>
+    )
+  }
+}
+
+function mapStateToProps({ authedUser }) {
+    return {
+        loading: authedUser === null
+    }
+}
+
+export default connect(mapStateToProps)(App)
